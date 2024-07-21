69fa659d1752dd6c1ff5e21d3c2fe310f95fd693
saikrishna5907 <saikrishna5907@gmail.com>
2024-07-21 20:59:56 +0100

    feat: created SAI COIN using ERC20

--- src/SaiToken.sol
+++ src/SaiToken.sol
@@ -1,37 +1,32 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-Licence-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
-contract SaiToken {
-    error InsufficientBalance(uint256 available, uint256 required);
-    mapping(address => uint256) private s_balances;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    function name() public pure returns (string memory) {
-        return "Sai Coin";
-    }
-
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
-    function totalSupply() public pure returns (uint256) {
-        return 1000 ether;
-    }
-
-    function balanceOf(address _owner) public view returns (uint256) {
-        return s_balances[_owner];
-    }
-
-    function transfer(address _to, uint256 _amount) public {
-        if (balanceOf(msg.sender) < _amount) {
-            revert InsufficientBalance({
-                available: balanceOf(msg.sender),
-                required: _amount
-            });
-        }
-        uint256 prevBalance = balanceOf(msg.sender) + balanceOf(_to);
-        s_balances[msg.sender] -= _amount;
-        s_balances[_to] += _amount;
-        require(balanceOf(msg.sender) + balanceOf(_to) == prevBalance);
+contract SaiToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Sai Coin", "SAITK") {
+        _mint(msg.sender, initialSupply);
     }
 }
---
+++ test/unit/SaiTokenTest.t.sol
@@ -0,0 +1,75 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DeploySaiToken} from "../../script/DeploySaiToken.s.sol";
+import {SaiToken} from "../../src/SaiToken.sol";
+
+contract SaiTokenTest is Test {
+    SaiToken public saiToken;
+    DeploySaiToken public saiTokenDeployer;
+
+    address address1 = makeAddr("address1");
+    address address2 = makeAddr("address2");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        saiTokenDeployer = new DeploySaiToken();
+        saiToken = saiTokenDeployer.run();
+
+        vm.prank(msg.sender);
+        saiToken.transfer(address1, STARTING_BALANCE);
+    }
+
+    function testAddress1Balance() public view {
+        assert(saiToken.balanceOf(address1) == STARTING_BALANCE);
+    }
+
+    function testAllowancesWorks() public {
+        uint256 initialAllowance = 1000;
+        uint256 transferAmount = 100;
+        //    address1 approves address2 to spend 1000 tokens
+        vm.prank(address1);
+        saiToken.approve(address2, initialAllowance);
+
+        //    address2 spends 100 tokens
+        vm.prank(address2);
+        saiToken.transferFrom(address1, address2, transferAmount);
+
+        //    address2 should have 100 tokens
+        assert(saiToken.balanceOf(address2) == transferAmount);
+
+        //    address1 should have 900 tokens
+        assert(saiToken.balanceOf(address1) == STARTING_BALANCE - transferAmount);
+
+        //    address2 should have 900 tokens left to spend
+        assert(saiToken.allowance(address1, address2) == initialAllowance - transferAmount);
+    }
+
+    function testTransfer() public {
+        uint256 transferAmount = 100;
+        //    address1 transfers 100 tokens to address2
+        vm.prank(address1);
+        saiToken.transfer(address2, transferAmount);
+
+        //    address2 should have 100 tokens
+        assert(saiToken.balanceOf(address2) == transferAmount);
+
+        //    address1 should have STARTING_BALANCE minus the transfered amount
+        assert(saiToken.balanceOf(address1) == STARTING_BALANCE - transferAmount);
+    }
+
+    function testBalanceAfterTransfer() public {
+        uint256 transferAmount = 100;
+        //    address1 transfers 100 tokens to address2
+        uint256 receiverInitialBalance = saiToken.balanceOf(address2);
+        vm.prank(address1);
+        saiToken.transfer(address2, transferAmount);
+
+        //    address1 should have STARTING_BALANCE minus the transfered amount
+        assert(saiToken.balanceOf(address1) == STARTING_BALANCE - transferAmount);
+        assertEq(receiverInitialBalance + transferAmount, saiToken.balanceOf(address2));
+    }
+}
